52558f41995a3eab781fc5b7d0bb90cbd88b5b5f
Aman <amanmaurya8419@gmail.com>
2022-10-19 15:27:51 +0530

    correct_prime_code

--- prime.js
+++ prime.js
@@ -1,16 +1,18 @@
 // taking a number
 let num=23;
+let count = 0;
 
 // checking wither number is prime or not
 for(let i=2; i<num; i++)
 {
 	if(num%i==0)
 	{
+		count = 1;
 		console.log("not Prime");
 		break;
 	}
 }
-if(i==num)
+if(count == 0)
 {
 	console.log("prime");
 }
